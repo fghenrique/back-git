c0e45ca3dd502a5ba4a765deca61959ca5e50b3c
lucas <galo_pc@outlook.com>
2020-09-20 11:23:00 -0300

    Controller usuarios

--- src/main/java/br/com/fabricio/apigit/controller/UsuariosController.java
+++ src/main/java/br/com/fabricio/apigit/controller/UsuariosController.java
@@ -1,5 +1,22 @@
 package br.com.fabricio.apigit.controller;
 
 
+import br.com.fabricio.apigit.models.Usuarios;
+import br.com.fabricio.apigit.service.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UsuariosController {
+
+    @Autowired
+    private UsuariosService usrService;
+
+    @GetMapping("user/list")
+    public List<Usuarios> list(){
+        return usrService.findAll();
+    }
 }
