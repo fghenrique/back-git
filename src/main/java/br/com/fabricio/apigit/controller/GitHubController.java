ef2683d647f0f6e4d08fbbf5763296110d452a45
fghenrique <fabricio.henrique@fatec.sp.gov.br>
2020-09-24 10:05:22 -0300

    Trocandno a versão java

--- src/main/java/br/com/fabricio/apigit/controller/GitHubController.java
+++ src/main/java/br/com/fabricio/apigit/controller/GitHubController.java
@@ -1,6 +1,7 @@
 package br.com.fabricio.apigit.controller;
 
 import br.com.fabricio.apigit.dataprovider.git.call.CallGitHub;
+import br.com.fabricio.apigit.dataprovider.git.dto.ProgramadorDTO;
 import br.com.fabricio.apigit.dataprovider.git.dto.ProgramadoresDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ public class GitHubController {
 
     @GetMapping("/list")
     public ProgramadoresDTO lista() {
-        var pg1 = callGitHub.Programador("https://api.github.com/search/users?q=repos:>50+followers:>100&page=1");
+        ProgramadoresDTO pg1 = callGitHub.Programador("https://api.github.com/search/users?q=repos:>50+followers:>100&page=1");
 
         ProgramadoresDTO progra = new ProgramadoresDTO();
         progra.setItems(pg1.getItems());
